7e29240dadf3f2db25748a036c49fa632f657de7
Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>
2018-01-29 16:30:09 +0100

    improved moveit testing node
    Signed-off-by: Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>

--- rll_moveit_testing/include/trajectory_sampler.h
+++ rll_moveit_testing/include/trajectory_sampler.h
@@ -1,7 +1,20 @@
 /*
- * This file is part of the Robot Learning Lab stack at KIT
+ * This file is part of the Robot Learning Lab stack
  *
- * TODO: clarify license and authorship with Udacity and their Pick and Place project
+ * Copyright (C) 2018 Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef RLL_TRAJECTORY_SAMPLER_H
@@ -15,21 +28,28 @@
 
 class TrajectorySampler
 {
- public:
-  explicit TrajectorySampler(ros::NodeHandle nh);
-  ~TrajectorySampler();
+public:
+	explicit TrajectorySampler(ros::NodeHandle nh);
+	~TrajectorySampler();
   
- private:
-  const std::string PLANNING_GROUP = "manipulator";
+private:
+	const std::string PLANNING_GROUP = "manipulator";
 
-  moveit::planning_interface::MoveGroupInterface move_group;
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-  bool success_plan;
+	moveit::planning_interface::MoveGroupInterface move_group;
+	moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+	bool success_plan;
 
-  iiwa_ros::iiwaRos my_iiwa;
+	iiwa_ros::iiwaRos my_iiwa;
 
-  void moveToPose(const geometry_msgs::Pose target_pose);
+	void moveToPose(const geometry_msgs::Pose target_pose);
 
 };
 
 #endif  // RLL_TRAJECTORY_SAMPLER_H
+
+/*
+ * Local Variables:
+ * c-file-style: "linux"
+ * indent-tabs-mode: t
+ * End:
+ */
