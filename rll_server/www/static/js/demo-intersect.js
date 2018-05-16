ed19b9f0efa4fec78516ceeb93fe499cf68a632e
Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>
2018-05-16 20:15:28 +0200

    demo pages: set current tags
    Signed-off-by: Wolfgang Wiedmeyer <wolfgang.wiedmeyer@kit.edu>

--- rll_server/www/static/js/demo-intersect.js
+++ rll_server/www/static/js/demo-intersect.js
@@ -172,7 +172,9 @@ $(function() {
 
 $("#hanoi_input").click(function() {
     $('input[name="project"]').val("tower_of_hanoi");
+    $('input[name="git_tag"]').val("v0.2");
 });
 $("#greetings_input").click(function() {
     $('input[name="project"]').val("greetings");
+    $('input[name="git_tag"]').val("v0.1");
 });
